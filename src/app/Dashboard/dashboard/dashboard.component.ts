d7d8a89e4d8f49285fce7f437fdc7822b5070625
BishalJung <52627217+BishalJung7377@users.noreply.github.com>
2021-09-30 10:08:32 +0545

    Dashboard is created
    Dashboard for viewing task, list , adding  task and list is created

---
+++ src/app/Dashboard/dashboard/dashboard.component.ts
@@ -0,0 +1,30 @@
+import { MmodalAddnewTaskComponent } from 'src/app/Modal/mmodal-addnew-task/mmodal-addnew-task.component';
+import { FormBuilder } from '@angular/forms';
+import { NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from 'src/app/Modal/modal/modal.component';
+import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  date =new Date().toLocaleString('en-us',{month:'long', year:'numeric', day:'numeric'})
+  modalRef!: MdbModalRef<MmodalAddnewTaskComponent>;
+  constructor(private formBuilder: FormBuilder, 
+    config: NgbModalConfig,
+    private modal: NgbModal,
+    private _modalService: NgbModal,
+    private modalService: MdbModalService) { 
+      
+    }
+  ngOnInit(): void {
+  }
+  openModal() {
+    this.modalRef = this.modalService.open(MmodalAddnewTaskComponent, {modalClass: 'modal-dialog-centered' })
+  }
+  
+}
---
+++ src/app/Dashboard/listname/listname.component.ts
@@ -0,0 +1,45 @@
+import { ListService } from './../../services/ListServices/list.service';
+import { FormBuilder } from '@angular/forms';
+import { NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from 'src/app/Modal/modal/modal.component';
+import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
+
+@Component({
+  selector: 'app-listname',
+  templateUrl: './listname.component.html',
+  styleUrls: ['./listname.component.css']
+})
+export class ListnameComponent implements OnInit {
+  panelOpenState = false;
+  collapse: boolean = true;
+  modalRef!: MdbModalRef<ModalComponent>;
+listData:any=[]
+listCounter: number= 0
+  constructor(private formBuilder: FormBuilder, 
+    config: NgbModalConfig,
+    private modal: NgbModal,
+    private _modalService: NgbModal,
+    private modalService: MdbModalService,
+    private ListDataName: ListService
+    ) { 
+      this.ListDataName.displayList().subscribe((response:any)=>{
+        this.listData = response
+        this.listCounter = this.listData.length,
+        console.log(  this.listCounter);
+      })
+    }
+  ngOnInit(): void {
+  }
+  open(content:any){
+    this.modal.open(content, { centered: true, windowClass: 'my-class'});
+
+  }
+  openModal() {
+    this.modalRef = this.modalService.open(ModalComponent, {
+      modalClass: 'modal-dialog-centered'
+    })
+  }
+
+}
